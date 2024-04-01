6b5ec9c673a9e1f0c89ecbe3c1de922c070ccbdf
AlexandraLukacs <lszandika15@gmail.com>
2024-04-01 17:24:17 +0200

    elírás javítás, megjelenítés javítás

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -1,20 +1,23 @@
 export function lista(){
     const lista = [];
-    for (let index = 0; index < 8; index++) {
+    for (let index = 0; index < 9; index++) {
         let szamok = Math.floor(Math.random()*2);
         lista.push(szamok);
     }
+    console.log(lista)
     return lista
 }
 
 export function osszeAllit(lista){
     const ELEM = $("#korok");
     let txt = " ";
-    for (let i = 0; i < lista.lenght; i++) {
+    for (let i = 0; i < lista.length; i++) {
         if(lista[i] === 0){
-            txt += `<div style="background-color : yellow"></div>`;
+            txt += `<div class=".elem" style="background-color : yellow"></div>`;
+            console.log(txt)
         }else{
-            txt += `<div style="background-color : green"></div>`;
+            txt += `<div class=".elem" style="background-color : green"></div>`;
+            console.log(txt)
         }
     }
 
